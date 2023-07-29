11c231ed2abe7c6bf999b563ddf025128f653010
vladislaw <vladmihin28@gmail.com>
2023-07-29 17:18:48 +0300

    rename videoId

--- internal/models/audio_storage.go
+++ internal/models/audio_storage.go
@@ -7,7 +7,7 @@ const AudioPath = "storage/audio/"
 type AudioStorage struct {
 	Id         uint `gorm:"primaryKey"`
 	PlaylistId uint
-	VideoId    string    `gorm:"type:varchar(255);index;not null"`
+	FileId     string    `gorm:"type:varchar(255);index;not null"`
 	Title      string    `gorm:"type:varchar(255);index;not null"`
 	Author     string    `gorm:"type:varchar(255);index;not null"`
 	FilePath   string    `gorm:"type:varchar(255);not null"`
